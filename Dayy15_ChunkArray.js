1f0acbd2bad62832676a9c868ae7ce0b9539fe09
Shubham Singh <76210714+Shubham7906@users.noreply.github.com>
2023-10-21 23:05:00 +0530

    30 Days of JS

---
+++ Dayy15_ChunkArray.js
@@ -0,0 +1,13 @@
+/**
+ * @param {Array} arr
+ * @param {number} size
+ * @return {Array}
+ */
+var chunk = function(arr, size) {
+    let chunked = []
+    for(let i = 0; i < arr.length; i+=size){
+        const ch = arr.slice(i, i+size)
+        chunked.push(ch)
+    }
+    return chunked
+};
